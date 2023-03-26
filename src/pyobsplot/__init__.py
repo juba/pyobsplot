07e71fc095271ab45a674eee058a1a4a83578385
Julien Barnier <julien@nozav.org>
2023-03-26 11:33:21 +0200

    fix: wrong imports after refactoring

--- src/pyobsplot/__init__.py
+++ src/pyobsplot/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.1.0"
 
-from .widget import Obsplot, Plot, d3, Math, js  # noqa:F401
+from .widget import Obsplot  # noqa:F401
+from .parsing import Plot, d3, Math, js  # noqa:F401
 
 __all__ = ["Obsplot", "Plot", "d3", "Math", "js", "__version__"]
