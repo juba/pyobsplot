ce88133b06b066298ff48bfed1e67f90a5b5787b
Julien Barnier <julien@nozav.org>
2023-04-01 22:53:12 +0200

    build: get __version__ from importlib

--- src/pyobsplot/__init__.py
+++ src/pyobsplot/__init__.py
@@ -1,4 +1,14 @@
-__version__ = "0.1.1"
+# Get __version__ from pyproject.toml (currently installed)
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:
+    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+
+try:
+    __version__ = version("anywidget")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
 
 from .widget import Obsplot  # noqa:F401
 from .parsing import Plot, d3, Math, js  # noqa:F401
