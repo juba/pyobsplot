ae3a5f31389583d88b232a956cc8bd498abf843a
Julien Barnier <julien@nozav.org>
2023-04-03 00:12:29 +0200

    chore: move ui test results and reports

--- tests/ui/playwright.config.cjs
+++ tests/ui/playwright.config.cjs
@@ -2,6 +2,8 @@ const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 
 module.exports = {
     ...baseConfig,
+    reporter: [['html', { outputFolder: 'playwright-report' }]],
+    outputDir: 'test-results',
     timeout: 2400000,
     retries: 0,
 };
