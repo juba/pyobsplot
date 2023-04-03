8755f5fed23e92d49108c2215ba416a3fe2e2c82
Julien Barnier <julien@nozav.org>
2023-04-03 15:41:53 +0200

    test: add jsdom visual tests

--- tests/ui/tests/pyobsplot.test.ts
+++ tests/ui/tests/pyobsplot.test.ts
@@ -2,13 +2,15 @@ import { expect, galata, test } from '@jupyterlab/galata';
 
 import * as path from 'path';
 
-async function test_notebook(page, notebook) {
+async function test_notebook(page, notebook, renderer) {
     const notebook_file = `${notebook}.ipynb`;
     await page.notebook.openByPath(notebook_file);
     await page.notebook.activate(notebook_file);
 
     const captures = new Array<Buffer>();
     const cellCount = await page.notebook.getCellCount();
+    // Replace first cell by Obsplot + set_renderer
+    await page.notebook.setCell(0, "code", `from pyobsplot import Obsplot, Plot, d3, Math, js; Obsplot.set_renderer('${renderer}'); print('${renderer}')`)
 
     await page.notebook.runCellByCell({
         onAfterCellRun: async (cellIndex: number) => {
@@ -21,8 +23,8 @@ async function test_notebook(page, notebook) {
 
     await page.notebook.save();
 
-    for (let i = 0; i < cellCount; i++) {
-        const image = `${notebook}-widgets-cell-${i}.png`;
+    for (let i = 0; i < (cellCount); i++) {
+        const image = `${renderer}-${notebook}-cell-${i}.png`;
         expect(captures[i]).toMatchSnapshot(image);
     }
 }
@@ -39,10 +41,14 @@ test.describe('Visual Regression', () => {
 
     let notebooks = ["syntax", "dates", "errors", "complex_plots", "data_sources", "transforms", "geo"];
 
-    for (let notebook of notebooks) {
-        test(`${notebook}.ipynb`, async ({ page }) => {
-            await test_notebook(page, notebook);
-        });
+    // check all renderers
+    for (let renderer of ["widget", "jsdom"]) {
+        // check all notebooks
+        for (let notebook of notebooks) {
+            test(`${renderer} / ${notebook}.ipynb`, async ({ page }) => {
+                await test_notebook(page, notebook, renderer);
+            });
+        }
     }
 
 });
