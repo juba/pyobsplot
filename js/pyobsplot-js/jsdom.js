4c6b9adc5c1d2de4b26b6de055da91eff258fb59
Julien Barnier <julien@nozav.org>
2023-04-04 10:49:53 +0200

    refactor: rename plot() to jsdom_plot()

--- js/pyobsplot-js/jsdom.js
+++ js/pyobsplot-js/jsdom.js
@@ -4,7 +4,6 @@
 
 import * as Plot from "@observablehq/plot"
 import * as d3 from "d3"
-import * as canvas from "canvas"
 import { JSDOM } from "jsdom"
 import { generate_plot } from "./plot.js"
 
@@ -17,12 +16,11 @@ global.Event = jsdom.window.Event
 global.Node = jsdom.window.Node
 global.NodeList = jsdom.window.NodeList
 global.HTMLCollection = jsdom.window.HTMLCollection
-
 // Make Plot and d3 available in js()
 global.d3 = d3
 global.Plot = Plot
 
-function plot(spec) {
+function jsdom_plot(spec) {
 
     spec = JSON.parse(spec)
     let el = generate_plot(spec, "jsdom")
@@ -47,7 +45,7 @@ process.stdin.on('data', function (chunk) {
     data += chunk;
 });
 process.stdin.on('end', function () {
-    let output = plot(data)
+    let output = jsdom_plot(data)
     process.stdout.write(output)
 });
 
