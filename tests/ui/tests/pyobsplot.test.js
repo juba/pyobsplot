9c1b23bf0a4fda18d9fe9bc446c95e4e8b5cac1a
Julien Barnier <julien@nozav.org>
2023-10-10 11:08:47 +0200

    test: remove jsdom renderer from ui tests

--- tests/ui/tests/pyobsplot.test.js
+++ tests/ui/tests/pyobsplot.test.js
@@ -10,11 +10,6 @@ async function test_notebook(page, notebook, renderer) {
     const captures = new Array();
     const cellCount = await page.notebook.getCellCount();
 
-    if (renderer) {
-        // Replace first cell by Obsplot + set_renderer
-        await page.notebook.setCell(0, "code", `from pyobsplot import Obsplot, Plot, d3, Math, js; op = Obsplot(renderer='${renderer}'); print('${renderer}')`)
-    }
-
     await page.notebook.runCellByCell({
         onAfterCellRun: async (cellIndex) => {
             const cell = await page.notebook.getCellOutput(cellIndex);
@@ -27,14 +22,19 @@ async function test_notebook(page, notebook, renderer) {
     await page.notebook.save();
 
     for (let i = 0; i < (cellCount); i++) {
-        const image = renderer ? `${renderer}-${notebook}-cell-${i}.png` : `${notebook}-cell-${i}.png`
+        const image = `${notebook}-cell-${i}.png`
         expect.soft(captures[i]).toMatchSnapshot(image);
     }
 }
 
 test.describe('Visual Regression', () => {
 
-    test.beforeEach(async ({ page, tmpPath }) => {
+
+    test.beforeEach(async ({ page }) =>
+        await page.filebrowser.openDirectory("/")
+    );
+
+    test("Upload notebooks and data to server", async ({ page, tmpPath }) => {
         await page.contents.uploadDirectory(
             path.resolve(path.resolve(''), `./tests/ui/tests/notebooks/`),
             '/'
@@ -42,40 +42,19 @@ test.describe('Visual Regression', () => {
         await page.filebrowser.openDirectory('/');
     });
 
-    // notebooks to test with each renderer
-    let notebooks = ["syntax", "dates", "errors", "complex_plots", "data_sources", "transforms", "geo"];
-    for (let renderer of ["widget", "jsdom"]) {
-        // check all notebooks
-        for (let notebook of notebooks) {
-            test(`${renderer} / ${notebook}.ipynb`, async ({ page }) => {
-                await test_notebook(page, notebook, renderer);
-            });
-        }
-    }
+    // notebooks to test
+    let notebooks = [
+        "syntax", "dates", "errors", "complex_plots", "data_sources",
+        "transforms", "geo", "mix_renderers", "default_values", "themes"
+    ];
 
-    // notebooks mixing renderers
-    let mix_notebooks = ["mix_renderers"]
-    for (let notebook of mix_notebooks) {
-        test(`Mix / ${notebook}.ipynb`, async ({ page }) => {
+    // check all notebooks
+    for (let notebook of notebooks) {
+        test(`General / ${notebook}.ipynb`, async ({ page }) => {
             await test_notebook(page, notebook, null);
         });
     }
 
-    // default notebook
-    let default_notebooks = ["default_values"]
-    for (let notebook of default_notebooks) {
-        test(`Default / ${notebook}.ipynb`, async ({ page }) => {
-            await test_notebook(page, notebook, null);
-        });
-    }
-
-    // themes
-    let themes_notebooks = ["themes"]
-    for (let notebook of themes_notebooks) {
-        test(`Default / ${notebook}.ipynb`, async ({ page }) => {
-            await test_notebook(page, notebook, null);
-        });
-    }
 
 
 });
