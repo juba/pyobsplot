b4145af63227183dd6c2ed56b3b4e6ef39eb6e54
Julien Barnier <julien@nozav.org>
2024-05-24 15:02:38 +0200

    fix: path and format combinations

--- src/pyobsplot/obsplot.py
+++ src/pyobsplot/obsplot.py
@@ -259,68 +259,6 @@ class ObsplotJsdomCreator(ObsplotCreator):
         self._proc = None
         self.start_server()
 
-    def __call__(
-        self,
-        *args,
-        **kwargs,
-    ) -> None:
-        """
-        Method called when an instance is called.
-        """
-        if self._proc is not None and self._proc.poll() is not None:
-            msg = "Server has ended, please recreate your plot generator object."
-            raise RuntimeError(msg)
-
-        # Extract configuration arguments from spec
-        path = None
-        if "path" in kwargs:
-            path = kwargs["path"]
-            del kwargs["path"]
-        if "format" in kwargs:
-            format = kwargs["format"]  # noqa: A001
-            del kwargs["format"]
-        else:
-            format = self.format  # noqa: A001
-        if "format_options" in kwargs:
-            format_options = kwargs["format_options"]
-            del kwargs["format_options"]
-        else:
-            format_options = self.format_options
-
-        if path is not None and not isinstance(path, io.StringIO):
-            extension = Path(path).suffix.lower()[1:]
-            allowed_extensions = ["html", "svg", "pdf", "png"]
-            if extension not in allowed_extensions:
-                msg = f"Output file extension should be one of {allowed_extensions}"
-                raise ValueError(msg)
-            # If both format and path are provided, use path extension
-            if format is not None and format != extension[1:]:
-                warnings.warn(
-                    f"Generating file in {extension} format based on file extension.",
-                    RuntimeWarning,
-                    stacklevel=1,
-                )
-                format = extension  # noqa: A001
-
-        spec = self.get_spec(*args, **kwargs)
-        if "figure" not in spec and format in ["html", "png", "pdf"]:
-            spec["figure"] = True
-        res = ObsplotJsdom(
-            spec,
-            port=self._port,
-            theme=self._theme,
-            default=self._default,
-            debug=self._debug,
-        ).plot()
-        if format in ["png", "pdf"]:
-            res = self.typst_render(res, format, format_options)
-        if format == "png":
-            res = Image(res)
-        if path is None:
-            display(res)
-        else:
-            ObsplotJsdomCreator.save_to_file(path, res)  # type: ignore
-
     def start_server(self):
         """
         Start http node plot generator server.
@@ -369,10 +307,84 @@ class ObsplotJsdomCreator(ObsplotCreator):
         if self._proc is not None:
             os.killpg(os.getpgid(self._proc.pid), signal.SIGTERM)
 
-    def typst_render(self, res, format, options) -> bytes:  # noqa: A002
+    def __call__(
+        self,
+        *args,
+        **kwargs,
+    ) -> None:
+        """
+        Method called when an instance is called.
+        """
+        if self._proc is not None and self._proc.poll() is not None:
+            msg = "Server has ended, please recreate your plot generator object."
+            raise RuntimeError(msg)
 
-        if format not in ["png", "pdf"]:
-            msg = "Invalid output: {output}."
+        # Extract configuration arguments from spec
+        path = None
+        if "path" in kwargs:
+            path = kwargs["path"]
+            del kwargs["path"]
+        if "format" in kwargs:
+            format = kwargs["format"]  # noqa: A001
+            del kwargs["format"]
+        else:
+            format = self.format  # noqa: A001
+        if "format_options" in kwargs:
+            format_options = kwargs["format_options"]
+            del kwargs["format_options"]
+        else:
+            format_options = self.format_options
+
+        allowed_formats = ["html", "png", "svg"]
+        if format not in allowed_formats:
+            msg = f"Invalid format: {format}.\n Allowed formats: {allowed_formats}."
+            raise ValueError(msg)
+
+        if path is not None and not isinstance(path, io.StringIO):
+            extension = Path(path).suffix.lower()[1:]
+            allowed_extensions = ["html", "svg", "pdf", "png"]
+            if extension not in allowed_extensions:
+                msg = f"Output file extension should be one of {allowed_extensions}"
+                raise ValueError(msg)
+            # If both format and path are provided, use path extension
+            if format is not None and format != extension:
+                warnings.warn(
+                    f"Generating file in {extension} format based on file extension.",
+                    RuntimeWarning,
+                    stacklevel=1,
+                )
+            format = extension  # noqa: A001
+
+        spec = self.get_spec(*args, **kwargs)
+        if "figure" not in spec and format in ["html", "png", "pdf"]:
+            spec["figure"] = True
+
+        res = ObsplotJsdom(
+            spec,
+            port=self._port,
+            theme=self._theme,
+            default=self._default,
+            debug=self._debug,
+        ).plot()
+
+        if format in ["png", "pdf"] or format == "svg" and isinstance(res, HTML):
+            if format == "svg" and isinstance(res, HTML):
+                warnings.warn(
+                    f"HTML figure converted to SVG via typst.",
+                    RuntimeWarning,
+                    stacklevel=1,
+                )
+            res = self.typst_render(res, format, format_options)
+
+        if path is None:
+            display(res)
+        else:
+            ObsplotJsdomCreator.save_to_file(path, res)  # type: ignore
+
+    def typst_render(self, res, format, options) -> SVG | Image | bytes:  # noqa: A002
+
+        if format not in ["png", "pdf", "svg"]:
+            msg = f"Invalid format: {format}."
             raise ValueError(msg)
 
         with tempfile.TemporaryDirectory() as tmpdirname:
@@ -401,9 +413,17 @@ class ObsplotJsdomCreator(ObsplotCreator):
                     typst_content += f"scale: {value},"
                 typst_content += ")"
                 typst_file.write(typst_content)
+
             typst.compile(input_file, output=output_file, ppi=100, format=format)
-            with open(output_file, "rb") as f:
+
+            mode = "rb" if format in ["png", "pdf"] else "r"
+            with open(output_file, mode) as f:
                 res = f.read()
+            if format == "png":
+                res = Image(res)
+            if format == "svg":
+                res = SVG(res)
+
         return res
 
     @staticmethod
@@ -421,10 +441,12 @@ class ObsplotJsdomCreator(ObsplotCreator):
         if isinstance(path, io.StringIO):
             path.write(str(res.data))
             return
-        extension = Path(path).suffix.lower()
-        if extension in [".html", ".svg"]:
-            with open(path, "w", encoding="utf-8") as f:
-                f.write(str(res.data))
-        else:
+        if isinstance(res, Image):
+            with open(path, "wb") as f:
+                f.write(res.data)  # type: ignore
+        if isinstance(res, bytes):
             with open(path, "wb") as f:
                 f.write(res)  # type: ignore
+        if isinstance(res, (HTML, SVG)):
+            with open(path, "w", encoding="utf-8") as f:
+                f.write(str(res.data))
