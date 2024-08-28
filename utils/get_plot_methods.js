0ab55e0f81281860b1bf280cb8c571ad52ddd756
Julien Barnier <julien@nozav.org>
2024-08-28 18:00:55 +0200

    feat: construct Plot methods code from Plot method names
    #13

--- utils/get_plot_methods.js
+++ utils/get_plot_methods.js
@@ -1,10 +1,35 @@
 #!/usr/bin/env node
 
 import * as Plot from "@observablehq/plot";
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get current file path
+const __filename = fileURLToPath(import.meta.url);
+const parentDir = path.dirname(path.dirname(__filename));
+
+async function replaceLines(filePath, newContent) {
+    let content = await fs.readFile(filePath, "utf8");
+    const regex =
+        /(    # STATIC PLOT METHODS START\n)[\s\S]*?(\n    # STATIC PLOT METHODS END\n)/;
+    content = content.replace(regex, `$1${newContent}\n$2`);
+    await fs.writeFile(filePath, content, "utf8");
+}
+
+function python_method(name) {
+    return `
+    @staticmethod
+    def ${name}(*args, **kwargs) -> dict: # noqa: ARG001, ARG004, RUF100
+        return method_to_spec("${name}", *args, **kwargs)`;
+}
 
 let methods = Object.keys(Plot)
-    .map((d) => '"' + d + '"')
-    .join(", ");
-methods = `PLOT_METHODS = (${methods})`;
+    .filter((d) => d[0] !== d[0].toUpperCase())
+    .filter((d) => d != "plot")
+    .map(python_method)
+    .join("\n");
 
-process.stdin.write(methods);
+replaceLines(parentDir + "/src/pyobsplot/js_modules.py", methods).catch(console.error);
+
+//process.stdin.write(methods);
